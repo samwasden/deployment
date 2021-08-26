fae78beff4cbe80623e410c040fa2b741b503330
Sam Wasden <samwasden@outlook.com>
2021-08-26 14:12:25 -0600

    edited css

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
 app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/main.js'))
+    res.sendFile(path.join(__dirname, 'public/main.js'))
 })
 
-app.use('/css', express.static(__dirname, 'public/css/style.css'));
+app.use('/css', express.static(__dirname, 'public/style.css'));
 
 const port = process.env.PORT || 4005;
 
