618e06c74461bc65e76c2790e1b0ed0bd3e255cd
Sam Wasden <samwasden@outlook.com>
2021-08-26 13:59:48 -0600

    edited css

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/main.js'))
 })
 
-app.use(express.static(__dirname + '../public'));
+app.use('/css', express.static(__dirname + '../public/style.css'));
 
 const port = process.env.PORT || 4005;
 
